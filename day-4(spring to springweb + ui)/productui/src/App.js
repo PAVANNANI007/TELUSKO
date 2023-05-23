ee5bff4b22c6d0cdc8eb349b8155f9f938165764
DODIYAM PAVAN KUMAR <68941005+PAVANNANI007@users.noreply.github.com>
2023-05-23 18:04:50 +0530

    DAY-4

---
+++ day-4(spring to springweb + ui)/productui/src/App.js
@@ -0,0 +1,98 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+import { colours } from 'nodemon/lib/config/defaults';
+
+const App = () => {
+  const [products, setProducts] = useState([]);
+  const [newProduct, setNewProduct] = useState({
+    name: '',
+    type: '',
+    place: '',
+    warranty: 0
+  });
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const response = (await axios.get('http://localhost:8080/products'));
+      setProducts(response.data);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setNewProduct({
+      ...newProduct,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const addProduct = async () => {
+    try {
+      await axios.post('http://localhost:8080/product', newProduct);
+      setNewProduct({
+        name: '',
+        type: '',
+        place: '',
+        warranty: 0
+      });
+      fetchProducts();
+    } catch (error) {
+      console.error('Error adding product:', error);
+    }
+  };
+
+  const fetchOutOfWarrantyProducts = async () => {
+    try {
+      const response = await axios.get('http://localhost:8080/outOfWarrantyProducts');
+      setProducts(response.data);
+    } catch (error) {
+      console.error('Error fetching out-of-warranty products:', error);
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title" >Product Telusko </h1>
+
+      <div className="form-container">
+        <h2>Add Product to Telusko</h2>
+        <form className="form">
+          <label className="form-label">Name:</label>
+          <input className="form-input" type="text" name="name" value={newProduct.name} onChange={handleInputChange} />
+
+          <label className="form-label">Type:</label>
+          <input className="form-input" type="text" name="type" value={newProduct.type} onChange={handleInputChange} />
+
+          <label className="form-label">Place:</label>
+          <input className="form-input" type="text" name="place" value={newProduct.place} onChange={handleInputChange} />
+
+          <label className="form-label">Warranty:</label>
+          <input className="form-input" type="number" name="warranty" value={newProduct.warranty} onChange={handleInputChange} />
+
+          <button className="form-button" type="button" onClick={addProduct}>Add</button>
+        </form>
+      </div>
+
+      <div className="product-list">
+        <h2>Products</h2>
+        <button className="filter-button" onClick={fetchProducts}>Add All products</button>
+        <button className="filter-button" onClick={fetchOutOfWarrantyProducts}>Products Out of Warranty </button>
+        <ul className="list">
+          {products.map((product) => (
+            <li key={product.id} className="list-item">
+              <strong className="product-info">Name:</strong> {product.name}, <strong className="product-info">Type:</strong> {product.type}, <strong className="product-info">Place:</strong> {product.place}, <strong className="product-info">Warranty:</strong> {product.warranty}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default App;
